3727f52c01c4df95573da7efabf83ddb68ed3f6b
dokhai64-svg <dokhai64@gmail.com>
2026-09-14 21:43:57 +0700

    Delete app/Http/Controllers/Admin directory

--- app/Http/Controllers/Admin/DashboardController.php
+++
@@ -1,3 +0,0 @@
-<?php
-namespace App\Http\Controllers\Admin; use App\Http\Controllers\Controller; use App\Models\{Article,Artist,Category,Media,Site};
-class DashboardController extends Controller { public function __invoke(){ $stats=['articles'=>Article::count(),'published'=>Article::where('status','published')->count(),'artists'=>Artist::count(),'categories'=>Category::count(),'media'=>Media::count(),'sites'=>Site::count()];$recent=Article::with(['site','author'])->latest()->take(8)->get();return view('admin.dashboard',compact('stats','recent')); } }
