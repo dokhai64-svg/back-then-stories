45429a71b1489465404871f062a6251fe73b7daf
dokhai64-svg <dokhai64@gmail.com>
2026-09-14 18:33:59 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,14 @@ use Illuminate\Support\Facades\URL;use App\Models\AdSlot; use App\Services\SiteR
 class AppServiceProvider extends ServiceProvider {
  public function register():void{}
  public function boot():void{if (app()->environment('production')) {
-    URL::forceScheme('https');
+    URL::forceScheme('https');$publicStorage = public_path('storage');
+
+if (!is_link($publicStorage) && !file_exists($publicStorage)) {
+    app('files')->link(
+        storage_path('app/public'),
+        $publicStorage
+    );
+}
 }
    View::composer('*',function($view){
      $site=null;$slots=collect(); try{ if(Schema::hasTable('sites')){$site=app(SiteResolver::class)->current(); if($site && Schema::hasTable('ad_slots')) $slots=AdSlot::where('site_id',$site->id)->where('enabled',true)->get()->keyBy('key'); }}catch(\Throwable $e){}
